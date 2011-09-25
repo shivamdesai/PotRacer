78acd0165a3e7163eec371d6cb58af754b0fcf22
Julian Ceipek <julian.ceipek@gmail.com>
2011-09-25 13:36:23 -0400

    Added all required Vendor requests to PICReader

--- PICReader/PICReader.py
+++ PICReader/PICReader.py
@@ -1,37 +1,47 @@
 import ctypes
 import time
 
-class PICReader:
+class Reader:
+
+    READ_POT    = 0x01
+    READ_BUTTON = 0x02
+    SET_LED     = 0x03
+    CLEAR_LED   = 0x04
+    SET_CONFIG  = 0x09
+
     def __init__(self):
-        self.__usb__ = ctypes.cdll.LoadLibrary('usb.dll')
-        self.__usb__.initialize()
-        self.__dev__ = self.__usb__.open_device(0x6666, 0x0003, 0)
+        self._usb = ctypes.cdll.LoadLibrary('usb.dll')
+        self._usb.initialize()
+        self._dev = self._usb.open_device(0x6666, 0x0003, 0)
 
-    def setRA0(self, param):
-        SET_DUTY = 0x04
-        return self.__performCommand__(GET_RA0, parameter=param, io='w', requestType='vendor', recipient='device')
+    def readPotentiometer(self, player):
+        """
+        player = 0 or 1
+        """
+        return self._performCommand(self.READ_POT, parameter=player, io='r', requestType='vendor', recipient='device')
 
-    def readRA2(self, param=0):
-        GET_RA2 = 0x03
-        return self.__performCommand__(GET_RA2, parameter=param, io='r', requestType='vendor', recipient='device')
+    def readButton(self, player):
+        """
+        player = 0 or 1
+        """
+        return self._performCommand(self.READ_BUTTON, parameter=player, io='r', requestType='vendor', recipient='device')
 
-    def readRA3(self, param=0):
-        GET_RA3 = 0x05
-        return self.__performCommand__(GET_RA3, parameter=param, io='r', requestType='vendor', recipient='device')
+    def setLED(self, player):
+        """
+        player = 0 or 1
+        """
+        return self._performCommand(self.SET_LED, parameter=player, io='w', requestType='vendor', recipient='device')
 
-    def setRA1on(self):
-        SET_RA1 = 0x01
-        return self.__performCommand__(SET_RA1, parameter=0, io='w', requestType='vendor', recipient='device')
-
-    def setRA1off(self):
-        CLR_RA1 = 0x02
-        return self.__performCommand__(CLR_RA1, parameter=0, io='w', requestType='vendor', recipient='device')
+    def clearLED(self, player):
+        """
+        player = 0 or 1
+        """
+        return self._performCommand(self.CLEAR_LED, parameter=player, io='w', requestType='vendor', recipient='device')
 
     def setConfig(self):
-        SET_CONFIG = 0x09
-        return self.__performCommand__(SET_CONFIG, parameter=1, io='w', requestType='standard', recipient='device')
+        return self._performCommand(self.SET_CONFIG, parameter=1, io='w', requestType='standard', recipient='device')
 
-    def __performCommand__(self, requestID, parameter=0, io='r', requestType='standard', recipient='device'):
+    def _performCommand(self, requestID, parameter=0, io='r', requestType='standard', recipient='device'):
 
         if io == 'r':
             transferDirection = '1'
@@ -62,22 +72,27 @@ class PICReader:
         else:
             raise ValueError, "Invalid recipient value specified. Can only be 'device', 'interface', 'endpoint', or 'other'"
 
-        buffer = ctypes.c_buffer(8)
+        buffer = ctypes.c_buffer(1)
 
-        ret = self.__usb__.control_transfer(self.__dev__, int(transferDirection + requestType + recipient, 2), requestID, parameter, 0, wLength, buffer)
-        
+        ret = self._usb.control_transfer(self._dev, int(transferDirection + requestType + recipient, 2), requestID, parameter, 0, wLength, buffer)
+
         if ret < 0:
             raise IOError, "Unable to perform vendor request."
         else:
-            return buffer.raw
+            return ord(buffer.raw[0])
 
     def close(self):
         usb.close_device(dev)
 
-
-
 if __name__ == "__main__":
-    reader = PICReader()
+    reader = Reader()
     while True:
-        print repr(reader.readRA3())
+        print "POT0: ", reader.readPotentiometer(0)
+        print "POT1: ", reader.readPotentiometer(1)
+        print "BTTN0: ", reader.readButton(0)
+        print "BTTN1: ", reader.readButton(1)
+        reader.setLED(1)
+        reader.clearLED(0)
         time.sleep(0.1)
+        reader.clearLED(1)
+        reader.setLED(0)
