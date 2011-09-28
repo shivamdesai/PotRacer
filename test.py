53f49ce1d3c6709e37dbf72b917bd3ce7a893d76
Julian Ceipek <julian.ceipek@gmail.com>
2011-09-28 16:31:31 -0400

    Camera movement tested. Movement is jerky, but this is a consequence of not separating logic and display, which will need to be handled differently in the actual game, anyway.

--- test.py
+++ test.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     clock = pygame.time.Clock()
 
     track = Track()
-    for t in xrange(10):
+    for t in xrange(50):
         track.addSegment()
 
     camP1 = Camera((0,0,Settings.SCREEN_WIDTH/2,Settings.SCREEN_HEIGHT),
     (0,0,Settings.SCREEN_WIDTH/2,Settings.SCREEN_HEIGHT))
     camP2 = Camera((0,0,Settings.SCREEN_WIDTH/2,Settings.SCREEN_HEIGHT), (Settings.SCREEN_WIDTH/2,0,Settings.SCREEN_WIDTH/2,Settings.SCREEN_HEIGHT))
 
-    camP2.left -= 100
+    camP2.left -= 200
 
     renderer.setTrack(track)
     renderer.addCamera(camP1)
@@ -36,7 +36,8 @@ if __name__ == "__main__":
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
 
-        camP2.top += timeDelta*0.1
+        camP2.top -= timeDelta*0.1
+        camP1.left -= timeDelta*0.05
 
         renderer.render(clock)
         timeDelta = clock.tick(Settings.MAX_FPS)
